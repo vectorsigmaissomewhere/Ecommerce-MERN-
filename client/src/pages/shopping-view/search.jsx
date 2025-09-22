77578b626ed39dbbedf13b4b4a2dfde962144e37
vectorsigmaissomewhere <anishchy501625@gmail.com>
2025-09-22 22:04:57 +0545

    search functionality done

--- client/src/pages/shopping-view/search.jsx
+++ client/src/pages/shopping-view/search.jsx
@@ -2,14 +2,28 @@ import { Input } from "@/components/ui/input";
 import { useEffect, useState } from "react";
 import { useSearchParams } from "react-router-dom";
 import { useDispatch, useSelector } from "react-redux";
+import {
+  getSearchResults,
+  resetSearchResults,
+} from "@/store/shop/search-slice";
+import { fetchProductDetails } from "@/store/shop/products-slice";
+import { addToCart, fetchCartItems } from "@/store/shop/cart-slice";
+import ProductDetailsDialog from "@/components/shopping-view/product-details";
+import ShoppingProductTile from "@/components/shopping-view/product-tile";
+import { toast } from "sonner";
 
 function SearchProducts() {
-    const [keyword, setKeyword] = useState("");
-    const [searchParams, setSearchParams] = useSearchParams();
-    const dispatch = useDispatch();
-    const { searchResults } = useSelector((state) => state.shopSearch);
+  const [keyword, setKeyword] = useState("");
+  const [openDetailsDialog, setOpenDetailsDialog] = useState(false);
+  const [searchParams, setSearchParams] = useSearchParams();
+  const dispatch = useDispatch();
+  const { searchResults } = useSelector((state) => state.shopSearch);
+  const { productDetails } = useSelector((state) => state.shopProducts);
 
-    useEffect(() => {
+  const { user } = useSelector((state) => state.auth);
+
+  const { cartItems } = useSelector((state) => state.shopCart);
+  useEffect(() => {
     if (keyword && keyword.trim() !== "" && keyword.trim().length > 3) {
       setTimeout(() => {
         setSearchParams(new URLSearchParams(`?keyword=${keyword}`));
@@ -21,22 +35,82 @@ function SearchProducts() {
     }
   }, [keyword]);
 
-    return (
-        <div className="container mx-auto md:px-6 px-4 py-8">
-            <div className="flex justify-center mb-8">
-                <div className="w-full flex items-center">
-                    <Input 
-                    value={keyword}
-                    name="keyword"
-                    onChange={(event)=>setKeyword(event.target.value)}
-                    className="py-6"
-                        placeholder="Search Products..."
-                    />
-                </div>
-            </div>
-            <div>search results</div>
+  function handleAddtoCart(getCurrentProductId, getTotalStock) {
+    console.log(cartItems);
+    let getCartItems = cartItems.items || [];
+
+    if (getCartItems.length) {
+      const indexOfCurrentItem = getCartItems.findIndex(
+        (item) => item.productId === getCurrentProductId
+      );
+      if (indexOfCurrentItem > -1) {
+        const getQuantity = getCartItems[indexOfCurrentItem].quantity;
+        if (getQuantity + 1 > getTotalStock) {
+          toast.error(`Only ${getTotalStock} items in stock`);  
+
+          return;
+        }
+      }
+    }
+
+    dispatch(
+      addToCart({
+        userId: user?.id,
+        productId: getCurrentProductId,
+        quantity: 1,
+      })
+    ).then((data) => {
+      if (data?.payload?.success) {
+        dispatch(fetchCartItems(user?.id));
+        toast.success("Product added to cart successfully");
+      }
+    });
+  }
+
+  function handleGetProductDetails(getCurrentProductId) {
+    console.log(getCurrentProductId);
+    dispatch(fetchProductDetails(getCurrentProductId));
+  }
+
+  useEffect(() => {
+    if (productDetails !== null) setOpenDetailsDialog(true);
+  }, [productDetails]);
+
+  console.log(searchResults, "searchResults");
+
+
+  return (
+    <div className="container mx-auto md:px-6 px-4 py-8">
+      <div className="flex justify-center mb-8">
+        <div className="w-full flex items-center">
+          <Input
+            value={keyword}
+            name="keyword"
+            onChange={(event) => setKeyword(event.target.value)}
+            className="py-6"
+            placeholder="Search Products..."
+          />
         </div>
-    )
+      </div>
+      {!searchResults.length ? (
+        <h1 className="text-5xl font-extrabold">No result found!</h1>
+      ) : null}
+      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-5">
+        {searchResults.map((item) => (
+          <ShoppingProductTile
+            handleAddtoCart={handleAddtoCart}
+            product={item}
+            handleGetProductDetails={handleGetProductDetails}
+          />
+        ))}
+      </div>
+      <ProductDetailsDialog
+        open={openDetailsDialog}
+        setOpen={setOpenDetailsDialog}
+        productDetails={productDetails}
+      />
+    </div>
+  )
 }
 
 export default SearchProducts;
